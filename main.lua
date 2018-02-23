a994cadd0364f022c18067e7129564776e0990e1
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-02-23 21:52:32 +0000

    Basic movement

--- main.lua
+++ main.lua
@@ -1,12 +1,31 @@
+local speed = 10
+
 function love.load()
+    x = 0
+    y = 0
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("right") then
+        x = x + speed * dt
+    end
 
+    if love.keyboard.isDown("left") then
+        x = x - speed * dt
+    end
+
+    if love.keyboard.isDown("up") then
+        y = y - speed * dt
+    end
+
+    if love.keyboard.isDown("down") then
+        y = y + speed * dt
+    end
 end
 
 function love.draw()
-
+    love.graphics.setColor(0, 191, 191, 255)
+    love.graphics.rectangle("fill", x, y, 200, 200)
 end
 
 function love.keypressed(key)
